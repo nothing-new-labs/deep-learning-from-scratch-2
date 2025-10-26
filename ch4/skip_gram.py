ff947b2ad5836327085f5c854f472c47951eb903
LiBinfeng <1204975323@qq.com>
2025-10-26 17:01:45 +0800

    update ch4 (#1)
    Co-authored-by: LiBinfeng <libinfeng@selectdb.com>

---
+++ ch4/skip_gram.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+import sys
+sys.path.append('..')
+from common.layers import *
+from ch4.negative_sampling_layer import NegativeSamplingLoss
+
+
+class SkipGram:
+    def __init__(self, vocab_size, hidden_size, window_size, corpus):
+        V, H = vocab_size, hidden_size
+        rn = np.random.randn
+
+        # 初始化权重
+        W_in = 0.01 * rn(V, H).astype('f')
+        W_out = 0.01 * rn(V, H).astype('f')
+
+        # 生成层
+        self.in_layer = Embedding(W_in)
+        self.loss_layers = []
+        for i in range(2 * window_size):
+            layer = NegativeSamplingLoss(W_out, corpus, power=0.75, sample_size=5)
+            self.loss_layers.append(layer)
+
+        # 将所有权重和梯度汇总到列表中
+        layers = [self.in_layer] + self.loss_layers
+        self.params, self.grads = [], []
+        for layer in layers:
+            self.params += layer.params
+            self.grads += layer.grads
+
+        # 将单词的分布表示设置为成员变量
+        self.word_vecs = W_in
+
+    def forward(self, contexts, target):
+        h = self.in_layer.forward(target)
+
+        loss = 0
+        for i, layer in enumerate(self.loss_layers):
+            loss += layer.forward(h, contexts[:, i])
+        return loss
+
+    def backward(self, dout=1):
+        dh = 0
+        for i, layer in enumerate(self.loss_layers):
+            dh += layer.backward(dout)
+        self.in_layer.backward(dh)
+        return None
